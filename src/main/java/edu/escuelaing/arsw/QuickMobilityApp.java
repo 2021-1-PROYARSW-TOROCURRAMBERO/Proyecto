dea57ed497bc4a9d07ae77b994a741671832634f
Alejandro Toro Daza <skullzo@outlook.com>
2021-02-17 19:49:50 -0500

    Código Fuente

--- src/main/java/edu/escuelaing/arsw/QuickMobilityApp.java
+++ src/main/java/edu/escuelaing/arsw/QuickMobilityApp.java
@@ -1,6 +1,29 @@
 package edu.escuelaing.arsw;
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+import spark.Request;
+import spark.Response;
 public class QuickMobilityApp {
-	public static String main(String[] args) {
-	    return ("Hello World!");
+	public static void main(String[] args) {
+		port(getPort());
+        get("/", (req, res) -> inputDataPage(req, res));
+	}
+	private static String inputDataPage(Request req, Response res) {
+        String pageContent
+                = "<!DOCTYPE html>"
+                + "<html>"
+                + "<title>Quick Mobility</title>"
+                + "<body>"
+                + "Hello World!"
+                + "</body>"
+                + "</html>";
+        return pageContent;
+    }
+	static int getPort() {
+	       if (System.getenv("PORT") != null) {
+	           return Integer.parseInt(System.getenv("PORT"));
+	       }
+	       return 4567;
 	}
 }
